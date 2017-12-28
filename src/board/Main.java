b38db40bcf741281b4cee092759c107645d2c628
מאיר <@LAPTOP-E05G26DL>
2017-12-28 15:41:22 +0200

    28/12/17

--- src/board/Main.java
+++ src/board/Main.java
@@ -1,20 +1,16 @@
-package Images;
+package board;
 
 import java.awt.Component;
 import java.awt.GridLayout;
 
 import javax.swing.JFrame;
 
+
 public class Main {
 
 	public static void main(String[] args)
 	{
 		BoardPanel bp = new BoardPanel();
-		JFrame frame = new JFrame("rolit");
-		frame.add(bp);
-		frame.setVisible(true);
-		frame.setSize(900, 900);
-
 		
 		//frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
 	}
